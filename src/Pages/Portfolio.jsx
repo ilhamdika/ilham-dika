1c6d2af2507fdd178b656b8b3604ae7d059846e3
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2024-01-07 22:33:08 +0700

    feat: add new url portfolio

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -25,39 +25,39 @@ export const Portfolio = () => {
             <TypeIt className="dark:text-white justify-center text-center items-center flex text-4xl my-10 font-medium mobile:">
                 Portfolio</TypeIt>
                 <h1 className="dark:text-white justify-center text-center items-center flex text-xl my-10 font-light">This is the result of my work</h1>
-         <div className="grid grid-cols-3 laptop:flex-row dekstop:flex-row tablet:flex-row mobile:h-auto mobile:grid-cols-1 my-10">
-                {/* {[1,2,3,4,5,6,7].map(i=> (
-                <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
-                key={i}
-                thumbnail={'https://static.vecteezy.com/system/resources/previews/000/330/430/original/vector-pencil-line-black-icon.jpg'}
-                title={'Web Developer'}
-                description={'Create a website for information, business etc., which can be accessed digitally'}
-                />
-                ))} */}
+                <div className="grid grid-cols-3 laptop:flex-row dekstop:flex-row tablet:flex-row mobile:h-auto mobile:grid-cols-1 my-10">
+                        {/* {[1,2,3,4,5,6,7].map(i=> (
+                        <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
+                        key={i}
+                        thumbnail={'https://static.vecteezy.com/system/resources/previews/000/330/430/original/vector-pencil-line-black-icon.jpg'}
+                        title={'Web Developer'}
+                        description={'Create a website for information, business etc., which can be accessed digitally'}
+                        />
+                        ))} */}
 
-                <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
-                thumbnail={Satpam}
-                title={'Web Satpam'}
-                description={'Web satpam with laravel framework and react js as frontend and tailwind css as styling web'}
-                href={'https://github.com/ilhamdika/company'}
-                />
+                        <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
+                        thumbnail={Satpam}
+                        title={'Web Satpam'}
+                        description={'Web satpam with laravel framework and react js as frontend and tailwind css as styling web'}
+                        href={'https://dakaranusantara.com/'}
+                        />
 
-                <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
-                thumbnail={webFilm}
-                title={'Web Film'}
-                description={'Web Film with laravel framework and react js as frontend and tailwind css as styling web and using payment gateway midtrans'}
-                href={'https://github.com/ilhamdika/watch-film'}
-                />
+                        <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
+                        thumbnail={webFilm}
+                        title={'Web Film'}
+                        description={'Web Film with laravel framework and react js as frontend and tailwind css as styling web and using payment gateway midtrans'}
+                        href={'https://github.com/ilhamdika/watch-film'}
+                        />
 
-                <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
-                thumbnail={webMarketplace}
-                title={'Web Marketplace'}
-                description={'Web Marketplace with PHP native and Bootstrap as styling web'}
-                href={'https://github.com/ilhamdika/marketplace-furniture'}
-                />
+                        <CardPortfolio className={'bg-slate-900 bg-opacity-50 rounded-xl'}
+                        thumbnail={webMarketplace}
+                        title={'Web Marketplace'}
+                        description={'Web Marketplace with PHP native and Bootstrap as styling web'}
+                        href={'https://github.com/ilhamdika/marketplace-furniture'}
+                        />
 
-                
-            </div>
+                        
+                </div>
         </div>
         }
     </>
