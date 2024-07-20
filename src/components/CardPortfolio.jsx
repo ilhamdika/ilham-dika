3151be7f576e6bc144721f4bd6dd54e957320d8c
ilhamdika <ilham27dika@gmail.com>
2024-07-20 15:47:08 +0700

    change target

--- src/components/CardPortfolio.jsx
+++ src/components/CardPortfolio.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export const CardPortfolio = ({className, thumbnail, title, description, href}) => {
   return (
     <div className={`bg-slate-200 mr-3 ml-3 my-2 mobile:h-100 mobile:w-full mobile:my-2 mobile:mx-auto` + className }>
-        <Link target="_blank" to={href}>
+        <Link to={href}>
         <img src={thumbnail} alt="Gambar" className="w-full mb-4 p-5 mt-10 mobile:auto mobile:w-full mobile:px-2 " />
         </Link>
-        <Link target="_blank" to={href}>
+        <Link to={href}>
         <h3 className="text-3xl mb-2 dark:text-white font-light text-center">
             {title}
         </h3>
